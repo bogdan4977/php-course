b9d4889629512e199d04f36610bed003abff71e9
Bogdan Sedovolosy <bogdan4977@gmail.com>
2017-10-15 14:34:42 +0300

    lesson 1 initial

---
+++ local.lesson1/index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='description' content='php lesson 1'>
+    <meta name='keywords' content=''>
+    <meta name='robots' content='index, follow'>
+    <meta name='author' content='sedovolosy.com'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>PHP Lesson#1</title>
+    <link href='https://fonts.googleapis.com/css?family=Michroma' rel='stylesheet'>
+    <link rel='stylesheet' href='css/normalize.css'>
+    <link rel='stylesheet' href='css/all.css'>
+    <title>lesson_1</title>
+</head>
+<body>
+<div class='page-wrapper'>
+    <nav class='navigation centered'>
+        <?php
+        echo "<ul class='menu'>";
+        echo "<li class='menu__item'><a href='#'>Home</a></li>";
+        echo "<li class='menu__item'><a href='#'>Catalog</a></li>";
+        echo "<li class='menu__item'><a href='#'>Blog</a></li>";
+        echo "<li class='menu__item'><a href='#'>News</a></li>";
+        echo "<li class='menu__item'><a href='#'>Who we Are</a></li>";
+        echo "<li class='menu__item'><a href='#'>Facts</a></li>";
+        echo "<li class='menu__item'><a href='#'>Contacts</a></li>";
+        echo "</ul>"; ?>
+    </nav>
+    <section class="big-banner full-width">
+        <p>faster</p>
+        <p>better</p>
+        <p>higher</p>
+    </section>
+    <div class='columns centered'>
+        <aside class="columns__sidebar columns__sidebar--left">
+            <div class="side-banner">
+                <div class="banner__text"><?php echo "Adverisment"; ?></div>
+            </div>
+        </aside>
+        <section class='columns-content'>
+            <p class='content__text'><?php echo "Hello PHP !!!"; ?></p>
+            <p class='content__text'><?php echo "PHP's testting "; ?></p>
+            <p class='content__text'><?php echo 'PHP\'s testting'; ?></p>
+        </section>
+    </div>
+</div>
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+<script src='js/main.js'></script>
+</body>
+</html>
