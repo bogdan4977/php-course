21e4374ccd786d1e1b9a0f4bcc902319ca1e51d2
BogdanNotebook <bogdan4977@gmail.com>
2017-10-20 21:18:47 +0300

    lesson 2 materials

---
+++ local.lesson2/index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='description' content='php lesson 1'>
+    <meta name='keywords' content=''>
+    <meta name='robots' content='index, follow'>
+    <meta name='author' content='sedovolosy.com'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>PHP Lesson#2</title>
+    <link href='https://fonts.googleapis.com/css?family=Michroma' rel='stylesheet'>
+    <link rel='stylesheet' href='css/normalize.css'>
+    <link rel='stylesheet' href='css/all.css'>
+    <title>lesson_2</title>
+</head>
+<body>
+<div class='page-wrapper'>
+    <h1 class="main-title">Lesson 2</h1>
+    <nav class='navigation centered'>
+        <?php
+        echo "<ul class='menu'>";
+        echo "<li class='menu__item'><a href='#'>Home</a></li>";
+        echo "<li class='menu__item'><a href='#'>Catalog</a></li>";
+        echo "<li class='menu__item'><a href='#'>Blog</a></li>";
+        echo "<li class='menu__item'><a href='#'>News</a></li>";
+        echo "<li class='menu__item'><a href='#'>Who we Are</a></li>";
+        echo "<li class='menu__item'><a href='#'>Facts</a></li>";
+        echo "<li class='menu__item'><a href='#'>Contacts</a></li>";
+        echo "</ul>"; ?>
+    </nav>
+    <section class="big-banner full-width">
+        <p class="big-banner__text">faster</p>
+        <p class="big-banner__text">better</p>
+        <p class="big-banner__text">higher</p>
+        <p class="big-banner__text">common</p>
+    </section>
+    <div class='columns centered'>
+        <aside class="columns__sidebar columns__sidebar--left">
+            <div class="side-banner">
+                <div class="banner__text"><?php echo "Adverisment"; ?></div>
+            </div>
+        </aside>
+        <section class='columns__content'>
+            <p class='content-text'><?php echo "Hello PHP !!!"; ?></p>
+            <p class='content-text'><?php echo "PHP's testting "; ?></p>
+            <p class='content-text'><?php echo 'PHP\'s testting'; ?></p>
+        </section>
+    </div>
+    <section class="centered">
+        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dolor facere iusto nobis! Adipisci assumenda eligendi, explicabo labore molestiae voluptatem.</h3>
+        <?php
+            $mark = 5;
+            $markLabel = "Your mark is ";
+        ?>
+        <h2>
+            <?php echo $mark > 3 ? $markLabel . "<em>lower than common</em>" : $markLabel . "<em> lower than common</em>"; ?>
+        </h2>
+    </section>
+</div>
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+<script src='js/main.js'></script>
+</body>
+</html>
