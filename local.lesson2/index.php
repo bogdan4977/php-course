eba5a3472bd2601e4d5da322aaf201fe120c81ea
BOGDAN-PC\Bogdan <bogdan4977@gmail.com>
2017-10-23 15:43:26 +0300

    finish tasks lesson_2

--- local.lesson2/index.php
+++ local.lesson2/index.php
@@ -169,7 +169,7 @@
             //rewrited
             $var = 1;
             $var += 12;
-            $var -=14;
+            $var -= 14;
             $var *= 5;
             $var /= 7;
             $var += 1;
@@ -186,7 +186,7 @@
         $seconds = "seconds";
         ?>
         <p class="content-block__text">
-            <em><?php echo ($hour . ':' . $minutes . ':' . $seconds);?></em>
+            <em><?php echo($hour . ':' . $minutes . ':' . $seconds); ?></em>
         </p>
     </section>
     <h2 class="main-title" title="Concatination short form">#6 Concatination short form</h2>
@@ -195,9 +195,9 @@
             <?php
             //initial
             $text = 'Я';
-            $text = $text .' хочу' ;
-            $text = $text .' знать' ;
-            $text = $text .' PHP!' ;
+            $text = $text . ' хочу';
+            $text = $text . ' знать';
+            $text = $text . ' PHP!';
             echo "<em>" . $text . "</em>";
             ?>
         </p>
@@ -205,16 +205,17 @@
             <?php
             //rewrited
             $text = 'Я';
-            $text .= ' хочу' ;
-            $text .= ' знать' ;
-            $text .= ' PHP!' ;
+            $text .= ' хочу';
+            $text .= ' знать';
+            $text .= ' PHP!';
             echo "<em>" . $text . "</em>";
             ?>
         </p>
     </section>
     <h2 class="main-title" title="$foo $bar">#7 $foo $bar</h2>
     <section class="content-block centered">
-        <i class="task-text">Задана переменная $foo = 'bar'; Создать переменную bar, в которой будет храниться число 10. Вывести число 10, используя только переменную $foo
+        <i class="task-text">Задана переменная $foo = 'bar'; Создать переменную bar, в которой будет храниться число 10.
+            Вывести число 10, используя только переменную $foo
         </i>
         <?php
         $foo = 'bar';
@@ -222,7 +223,118 @@
         ?>
         <p class="content-block__text">Our number: <?php echo $$foo; ?></p>
     </section>
+    <h2 class="main-title" title="Calculations">#8 Calculations</h2>
+    <section class="content-block centered">
+        <?php
+        $num1 = 12;
+        $num2 = 34.5;
+        ?>
+        <p class="content-block__text"><code>$num1 = 12; $num2 = 34.5;</code></p>
+        <p class="content-block__text">num1 + num2 = <em><?php echo($num1 + $num2); ?></em></p>
+        <p class="content-block__text">num1 * num2 = <em><?php echo($num1 * $num2); ?></em></p>
+    </section>
+    <section class="content-block centered">
+        <?php
+        $num1 = 25;
+        $num2 = 13;
+        ?>
+        <p class="content-block__text"><code>$num1 = 25; $num2 = 13;</code></p>
+        <p class="content-block__text">num1^2 + num2^2 = <em><?php echo(pow($num1, 2) + pow($num2, 2)); ?></em></p>
+    </section>
+    <section class="content-block centered">
+        <?php
+        $num1 = 19;
+        $num2 = 37;
+        $num3 = 67;
+        $middleAr = ($num1 + $num2 + $num3) / 3;
+        ?>
+        <p class="content-block__text"><code>$num1 = 19; $num2 = 37; $num2 = 67; $middleAr = ($num1 + $num2 + $num3) /
+                3;</code></p>
+        <p class="content-block__text">$middleAr = <em><?php echo($middleAr); ?></em></p>
+    </section>
+    <section class="content-block centered">
+        <?php
+        $x = 4;
+        $y = 6;
+        $z = 9;
+        $expr = ($x + 1) - 2 * ($z - 2 * $x + $y);
+        ?>
+        <i class="task-text">Задана переменная $foo = 'bar'; Создать переменную bar, в которой будет храниться число 10.
+            Вывести число 10, используя только переменную $foo</i>
+        <p class="content-block__text"><code>$x = 4; $y = 6; $z = 9; <br> $expr = ($x + 1) - 2*($z - 2*$x + $y);</code>
+        </p>
+        <p class="content-block__text">$expr = <em><?php echo($expr); ?></em></p>
+    </section>
+    <section class="content-block centered">
+        <?php
+        $num1 = 79;
+        $num2 = 45;
+        $divideModule1 = $num1 % 3;
+        $divideModule2 = $num2 % 3;
 
+        $increaseNum1 = ($num1 * 1.3);
+        $increaseNum2 = ($num2 * 2.2);
+        ?>
+        <i class="task-text">Дано натуральное числа. Найдите остатки от деления этих чисел на 3 и на 5. Дано число.
+            Увеличьте его на 30%, на 120%.</i>
+        <p class="content-block__text"><code>$num1 = 79; $num2 = 45; $divideModule1 = $num1 % 3; $divideModule2 = $num2
+                % 3;</code></p>
+        <p class="content-block__text"><code>$increaseNum1 = ($divideModule1 * 1.3); $increaseNum2 = ($divideModule2 *
+                2.2);</code></p>
+        <p class="content-block__text">$divideModule1 = <em><?php echo $divideModule1; ?></em></p>
+        <p class="content-block__text">$divideModule2 = <em><?php echo $divideModule2; ?></em></p>
+        <p class="content-block__text">$increaseNum1 = <em><?php echo $increaseNum1; ?></em></p>
+        <p class="content-block__text">$increaseNum2 = <em><?php echo $increaseNum2; ?></em></p>
+    </section>
+    <section class="content-block centered">
+        <?php
+        $num1 = 34;
+        $num2 = 89;
+        //sum
+        $sum = ($num1 * 0.4) + ($num2 * 1.2);
+        ?>
+        <i class="task-text">Дано натуральное числа. Найдите остатки от деления этих чисел на 3 и на 5. Дано число.
+            Увеличьте его на 30%, на 120%.
+        </i>
+        <p class="content-block__text"><code>$num1 = 34; $num2 = 89; $sum = ($num1 * 0.4) + ($num2 * 1.2);</code></p>
+        <p class="content-block__text">$sum = <em><?php echo $sum; ?></em></p>
+    </section>
+    <section class="content-block centered">
+        <?php
+
+        ?>
+        <i class="task-text">Дано трехзначное числа. Найдите сумму его цифр</i>
+
+    </section>
+    <section class="content-block centered">
+        <?php
+        $num = 258;
+        $newDigit = 0;
+        //convert to sting
+        $numToString = (string)$num;
+        //change digit
+        $numToString[1] = $newDigit;
+        //get new number
+        $getNewNumber = (int)$numToString;
+        //reverse
+        $getReverceNumber = (int)($numToString[2] . $numToString[1] . $numToString[0]);
+        ?>
+        <p class="content-block__text">
+            <code>
+                <br>
+                <i>$num = 258;</i><br>
+                <i>$newDigit = 0;</i><br>
+                //convert to sting<br>
+                <i>$numToString = (string)$num;</i><br>
+                //chsange digit<br>
+                <i>$numToString[1] = $newDigit;</i><br>
+            </code>
+        </p>
+        <i class="task-text">Дано трехзначное число. Поменяйте среднюю цифру на ноль. Найдите число, полученное выписыванием в обратном порядке цифр данного трехзначного натурального числа.</i>
+        <p class="content-block__text">Initial number = <em><?php echo $num; ?></em></p>
+        <p class="content-block__text">Changed number = <em><?php echo (int)$numToString; ?></em></p>
+        <p class="content-block__text">Changed direction = <em><?php echo (int)($numToString[2] . $numToString[1] . $numToString[0]); ?></em></p>
+    </section>
 </div>
 <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
 <script src='js/main.js'></script>
