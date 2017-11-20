7991d59881ff89b444be8c2804b4dfa2e8516a03
BOGDAN-PC\Bogdan <bogdan4977@gmail.com>
2017-11-20 17:29:19 +0200

    mysql tasks

--- lesson6.mysql/form_product.php
+++ lesson6.mysql/form_product.php
@@ -23,11 +23,35 @@ $show_price = "";
 $show_model = "";
 $show_qty = "";
 
-if (isset($p_name) && $p_name && isset($p_price) && isset($p_model) && isset($p_qty)){
-    $show_name = $p_name;
-    $show_price = $p_price;
-    $show_model = $p_model;
-    $show_qty = $p_qty;
+$message = "Enter data to add product";
+
+if (isset($p_name) && $p_name && isset($p_price) &&($p_price) && isset($p_model) && isset($p_qty) && ($p_qty)){
+    /*database connect*/
+    $connect = mysqli_connect($host, $user, $password, $database) or die("Error" . mysqli_error($connect));
+
+    /*escapes special characters*/
+    $name = htmlentities(mysqli_real_escape_string($connect, strtolower($p_name)));
+    $price = htmlentities(mysqli_real_escape_string($connect, $p_price));
+    $model = htmlentities(mysqli_real_escape_string($connect, strtolower($p_model)));
+    $qty = htmlentities(mysqli_real_escape_string($connect, $p_qty));
+
+    /*query*/
+    $query = "INSERT INTO oc_product(prod_name, model, quantity, price, date_added) VALUES('$name', '$model', $qty, $price, NOW())";
+
+    /*get result*/
+    $result = mysqli_query($connect, $query) or die("Error" . mysqli_error($connect));
+
+    if($result){
+        $message = "Product '$name' was added";
+        $show_name = $p_name;
+        $show_price = $p_price;
+        $show_model = $p_model;
+        $show_qty = $p_qty;
+    }
+
+    /*close connection*/
+    mysqli_close($connect);
+
 } else {
     $show_name = 'Enter data';
 }
@@ -41,27 +65,29 @@ if (isset($p_name) && $p_name && isset($p_price) && isset($p_model) && isset($p_
     <a class="action-button" href="index.php">back</a>
 </section>
 <h2 class="main-title" title="Variables">Functions task</h2>
-
+<section class="content-block centered ">
+    <p class="content-block__text">Status: <i><?php echo $message; ?></i></p>
+</section>
 <section class="content-block centered ">
     <h2 class="content-block__title">Product data</h2>
     <form name="form_product" action="form_product.php" method="post" class="form" enctype="multipart/form-data">
 
-        <div class="form__element">
+        <div class="form__element necessary">
             <label for="user-number">Name</label>
-            <input id="user-number" name="p_name" placeholder="name" type="text" value=""/>
+            <input id="user-number" name="p_name" placeholder="name" type="text" value="<?php echo $p_name;?>"/>
         </div>
 
-        <div class="form__element">
+        <div class="form__element necessary">
             <label for="user-number">Price</label>
-            <input id="user-number" name="p_price" placeholder="price" type="number" value="" step="any"/> UAH
+            <input id="user-number" name="p_price" placeholder="price" type="number" value="<?php echo $p_price; ?>" step="any"/> UAH
         </div>
 
         <div class="form__element">
-            <label for="user-number">Price</label>
-            <input id="user-number" name="p_model" placeholder="model" type="text" value=""/>
+            <label for="user-number">Model</label>
+            <input id="user-number" name="p_model" placeholder="model" type="text" value="<?php echo $p_model; ?>"/>
         </div>
 
-        <div class="form__element">
+        <div class="form__element necessary">
             <label for="user-number">QTY</label>
             <input id="user-number" name="p_qty" placeholder="qty" type="number" value="" step="any"/>
         </div>
