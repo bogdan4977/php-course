52242f02add788ea89cdaf5453a7f00784c544fe
BogdanNotebook <bogdan4977@gmail.com>
2017-12-11 20:56:58 +0200

    regular expressions

---
+++ local.regular/index.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Get HEADER
+ */
+require_once __DIR__ . '/_header.php';
+?>
+
+<h1 class="main-title">Regular definitions</h1>
+<?php /*
+    <!-- page layout -->
+    <nav class='navigation centered'>
+        <?php
+        echo "<ul class='menu'>";
+        echo "<li class='menu__item'><a href='#'>Home</a></li>";
+        echo "<li class='menu__item'><a href='#'>Catalog</a></li>";
+        echo "<li class='menu__item'><a href='#'>Blog</a></li>";
+        echo "<li class='menu__item'><a href='#'>News</a></li>";
+        echo "<li class='menu__item'><a href='#'>Who we Are</a></li>";
+        echo "<li class='menu__item'><a href='#'>Facts</a></li>";
+        echo "<li class='menu__item'><a href='#'>Contacts</a></li>";
+        echo "</ul>"; ?>
+    </nav>
+    <section class="big-banner full-width">
+        <p class="big-banner__text">faster</p>
+        <p class="big-banner__text">better</p>
+        <p class="big-banner__text">higher</p>
+        <p class="big-banner__text">common</p>
+    </section>
+
+    <div class='columns centered'>
+        <aside class="columns__sidebar columns__sidebar--left">
+            <div class="side-banner">
+                <div class="banner__text"><?php echo "Adverisment"; ?></div>
+            </div>
+        </aside>
+        <section class='columns__content'>
+            <p class='content-text'><?php echo "Hello PHP !!!"; ?></p>
+            <p class='content-text'><?php echo "PHP's testting "; ?></p>
+            <p class='content-text'><?php echo 'PHP\'s testting'; ?></p>
+        </section>
+    </div>
+    */ ?>
+<?php
+/*$string = 'April 15, 2003';
+$pattern = '/(\w+) (\d+), (\d+)/i';
+$replacement = '${1}1,$3';*/
+
+/*echo '{startDate} = 1999-5-27<br/><br>';
+
+$patterns = array('/^\s*{(\w+)}\s*=/');
+$replace = array('$\1 =');
+echo preg_replace($patterns, $replace, '{startDate} = 1999-5-27');
+echo '<br><br>';*/
+$textbody = "This book is *very* difficult to find.";
+$word = "*very*";
+$textbody = preg_replace("/" . preg_quote($word) . "/","<b>" . $word . "</b>",$textbody);
+
+/**
+** 1) Создать таблицу отзывов, записать отзыв, достать отзыв заменить матюки (без регулярок)
+ */
+?>
+
+<section class="content-block centered ">
+    <a class="action-button" href="/">Reset</a>
+</section>
+
+<section class="content-block centered">
+    <p class="content-block__text"><i>initial:</i><?php echo $textbody; ?></p>
+    <p class="content-block__text"><i>result:</i></p>
+</section>
+
+
+<?php
+/**
+ * Get FOOTER
+ */
+require_once __DIR__ . '/_footer.php';
+?>
